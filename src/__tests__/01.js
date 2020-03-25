7a1888def58c0ff47dea22043d193264faebede3
Richard Hefner <brassman@gmail.com>
2020-03-25 09:06:38 -0700

    Exercise 1

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import {render, screen, fireEvent} from '@testing-library/react'
-import App from '../final/01'
-// import App from '../exercise/01'
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+// import App from '../final/01'
+import App from '../exercise/01';
 
 test('typing a name shows a greeting', () => {
-  render(<App />)
-  fireEvent.change(screen.getByLabelText(/name/i), {target: {value: 'bob'}})
-  screen.getByText(/hello.*bob/i)
-})
+  render(<App />);
+  fireEvent.change(screen.getByLabelText(/name/i), { target: { value: 'bob' } });
+  screen.getByText(/hello.*bob/i);
+});
--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,29 +1,28 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import React from 'react'
+import React from 'react';
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({ initialName = '' }) {
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
     <div>
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" placeholder="Fill me in.." />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Rich" />;
 }
 
-export default App
+export default App;
